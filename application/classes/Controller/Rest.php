f4e2b17c146b0e2e528aa87e7ed3f5f2da88492e
Nikhil Deshpande <niks.rd@gmail.com>
2018-11-25 12:15:56 +0530

    Udpates

--- application/classes/Controller/Rest.php
+++ application/classes/Controller/Rest.php
@@ -4,14 +4,16 @@ class Controller_Rest extends Controller {
 
 	public function action_index() {
         session_start();
-        if(!isset($_SERVER['HTTP_X_GOOG_RESOURCE_ID'])) {
+        if(!(isset($_SERVER['HTTP_X_GOOG_RESOURCE_ID']) || isset($_SERVER['HTTP_X_GOOG_RESOURCE_STATE']))) {
             App::out("Failed", false, 400);
         }
         
-        $resource_id = $_SERVER['HTTP_X_GOOG_RESOURCE_ID'];
-        $user_event_model = new Model_Events;
-        $user_event_model->delete_events_for_resource($resource_id);
-        
+        if($_SERVER['HTTP_X_GOOG_RESOURCE_STATE'] != 'sync') {
+            $resource_id = $_SERVER['HTTP_X_GOOG_RESOURCE_ID'];
+            $user_event_model = new Model_Events;
+            $user_event_model->delete_events_for_resource($resource_id);
+        }
+
         App::out("Ok");
 	}
 }
